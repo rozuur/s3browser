ea72fe1b589a2531a0591192dd42946d948a883b
Naveen Vardhi <vardhi.naveen@gmail.com>
2019-10-12 11:47:39 +0530

    develop local script for running application

---
+++ develop_local.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -euox pipefail
+IFS=$'\n\t'
+
+declare -r SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+declare -r APP=s3browse
+
+function static_analyse {
+    return 0
+}
+
+function venv_activate {
+    set +x
+    source "${SCRIPT_DIR}/.${APP}_venv/"bin/activate
+    echo "Using $(which python)"
+    echo "Using $(which pip)"
+    set -x
+}
+
+function build {
+    make -s -f "${SCRIPT_DIR}"/setup_osx_venv.mk
+    venv_activate
+    pip install -q \
+        -r "${SCRIPT_DIR}"/requirements/requirements.txt \
+        -r "${SCRIPT_DIR}"/requirements/dev-requirements.txt
+}
+
+function run {
+    local port="$1"
+    trap static_analyse EXIT
+    env FLASK_APP=s3browser.py flask run --host=0.0.0.0 --port="${port}"
+}
+
+function main {
+    local port=${1?Port is required}
+    unset PYTHONPATH
+    export PYTHONPATH="."
+    build
+    run "${port}"
+}
+
+main "$@"
+
